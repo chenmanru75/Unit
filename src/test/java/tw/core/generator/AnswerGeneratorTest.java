439d3913e27c81bed8db906c65afbbf05149f414
chenmanru75 <chenmanru75@163.com>
2019-05-28 11:09:56 +0800

    AnswerGenerator ok

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,29 @@
 package tw.core.generator;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+    private AnswerGenerator answer;
+    private RandomIntGenerator randomIntGenerator;
 
+    @Before
+    public void setup() {
+        randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+        answer = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Test
+    public void should_print_generate_actual_answer_when_the_game_begins() throws Exception {
+        assertEquals("1 2 3 4", answer.generate().toString());
+    }
 }
 
